9f3f426905088184c8464e59fff47c1055ff63e9
Bima710 <moch.bima@student.umn.ac.id>
2024-08-28 21:40:01 +0700

    git
    first

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Moch.Bima - 00000045997</Text>
+    </View>
+  );
+}
